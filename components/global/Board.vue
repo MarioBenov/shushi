69d611cbb9995edcc1737655c55cfe5e565ef7c9
Mario Benov <mario.ben@mail.bg>
2023-07-27 23:29:04 +0300

    feat: first commit

---
+++ components/global/Board.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="">
+        <div 
+            v-for="(row, rIndex) in board" 
+            :key="`row-${rIndex}`" 
+            class="flex"
+        >
+            <cell 
+                v-for="(cell, cIndex) in row" 
+                :key="`${rIndex}-${cIndex}`" 
+                :color="cell.color" 
+                :selected="_isMatch(selectedCells[0], [rIndex, cIndex])" 
+                :row="rIndex"
+                :col="cIndex"
+                @click="selectCell(rIndex, cIndex)"
+            />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import _isMatch from 'lodash/isMatch'
+import _ from 'lodash'
+import {CellColor} from '~/types'
+
+const props = defineProps<{
+    board: {color: CellColor}[][]
+}>()
+
+const emit = defineEmits<{
+    (e: 'swap', first: [number, number], second: [number, number]): void
+}>()
+
+let selectedCells = ref<[number, number][]>([])
+
+function selectCell(rIndex: number, cIndex: number) {
+    if(_.includes(selectedCells, _.isEqual)) {
+        selectedCells.value = []
+        return
+    }
+    
+    selectedCells.value.push([rIndex, cIndex])
+
+    if(selectedCells.value.length < 2) return
+    
+    const diffX = Math.abs(cIndex - selectedCells.value[0][1])
+    const diffY = Math.abs(rIndex - selectedCells.value[0][0])
+
+    if(diffX + diffY == 1)
+        emit('swap', selectedCells.value[0], selectedCells.value[1])
+    
+    selectedCells.value = []
+}
+</script>
---
+++ types/index.ts
@@ -0,0 +1,8 @@
+export enum CellColor {
+    RED = 'red',
+    GREEN = 'green',
+    BLUE = 'blue',
+    YELLOW = 'yellow',
+    PINK = 'pink',
+    EMPTY = 'empty'
+}
